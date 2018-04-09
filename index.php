cf11cd86106db87be025a9a0ef746c53ef1e7f3f
Kirill <makarih0@mail.ru>
2018-04-09 14:05:31 +0300

    Добавил верстку лендинга

---
+++ index.php
@@ -0,0 +1,258 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href = "Holderlin.css" rel = "stylesheet">
+    <link rel="shortcut icon" href="favicon.ico">
+    <title>Holderlin</title>
+</head>
+<body>
+    <div class = "contacts">
+        <ul class = "socialnetworks">
+            <li><img src="Holderlin/img/g+.jpg" alt=""></li>
+            <li> <img src="Holderlin/img/tw.jpg" alt=""></li>
+            <li><img src="Holderlin/img/f.jpg" alt=""></li>
+            <li><img src="Holderlin/img/p.jpg" alt=""></li>
+            <li><img src="Holderlin/img/in.jpg" alt=""></li>
+        </ul>
+        <ul class = "info">
+            <li><img src="Holderlin/img/map.jpg" alt="">8901 Marmora Road, Glasgow, DO4 89GR.</li>
+            <li><img src="Holderlin/img/phone.jpg" alt="">+1 959 6035 (585-SUPPORT)</li>
+            <li> <img src="Holderlin/img/mail.jpg" alt="">contact@demolink.org</li>
+        </ul>
+    </div>
+    <header>
+        <img src = "Holderlin/img/logo.png">
+        <nav>
+            <ul>
+                <li><span>Home</span></li>
+                <li><span>About</span></li>
+                <li><span>Pages</span></li>
+                <li><span>Blog</span></li>
+                <li><span>Gallery</span></li>
+                <li><span>Contacts</span></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <main>
+        <div class = "team">
+           <p> MEET THE TEAM</p>
+           <div class = "team-people">
+                <figure>
+                    <img src="Holderlin/img/team-1.jpg" alt="">
+                    <figcaption></figcaption>
+                    <figcaption>
+                            <p>JOHN FRANKLIN</p>
+                            <p>The founder of company, the head of the company.</p>
+                    </figcaption>
+                </figure>
+                <figure>
+                    <img src="Holderlin/img/team-2.jpg" alt="">
+                    <figcaption></figcaption>
+                    <figcaption>
+                        <p>BARBARA NILSON</p>
+                        <p>First subprincipal, lecturer. First-rate organizer and curator of company.</p>
+                    </figcaption>
+                    </figure>
+                    <figure>
+                        <img src="Holderlin/img/team-3.jpg" alt="">
+                        <figcaption></figcaption>
+                        <figcaption>
+                            <p>MARK POOL</p>
+                            <p>One of the leading company coaches and trainers.</p>
+                        </figcaption>
+                    </figure>
+                    <figure>
+                        <img src="Holderlin/img/team-4.jpg" alt="">
+                        <figcaption></figcaption>
+                        <figcaption>
+                            <p> JESSICA RIGHT </p>
+                            <p> Highly reputed and honored consultant.</p>
+                        </figcaption>
+                    </figure>
+            </div>
+
+
+        </div>
+        <div class = "welcome">
+            <h1>WELCOME!</h1>
+            <p> Make your company a world wide known name with us.<br> See what we can do for your business!</p>           
+            <figure>
+                <div class = "wrapper">
+                    <img src="Holderlin/img/marketing.jpg" alt="">
+                </div>
+                <figcaption>
+                        <p>MARKETING PROGRAMS</p>
+                        <p>Learn all the aspects of marketing.</p>
+                </figcaption>
+            </figure>
+            <figure>
+                <div class = "wrapper">
+                        <img src="Holderlin/img/premier.jpg" alt="">
+                </div>
+                <figcaption>
+                    <p>PREMIER SUPPORT</p>
+                    <p>Providing essential support service including 24/7.</p>
+                </figcaption>
+            </figure>
+            <figure>
+                <div class = "wrapper">
+                    <img src="Holderlin/img/services.jpg" alt="">
+                </div>
+                <figcaption>
+                    <p>SERVICES</p>
+                    <p>We can easily help your business get back onits feet.</p>
+                </figcaption>
+            </figure>
+            <figure>
+                <div class = "wrapper">
+                    <img src="Holderlin/img/fresh-ideas.jpg" alt="">
+                </div>
+                <figcaption>
+                    <p>FRESH IDEAS</p>
+                    <p>We want to share with the world fresh business ideas.</p>
+                </figcaption>
+            </figure>
+        </div>
+
+        <div class = "offer">
+            <div>
+                <h2>Save time, save money, grow and succeed!</h2>
+                <p>We provide professional customer care and excellent consulting services to build your business growth & prosperity. We are focused on providing you with the best-in-class care and services through world class technology and response mechanisms.</p>
+            </div>
+            <div>Click here</div>
+        </div>
+
+        <div class = "whatwedo">
+            <h2>WHAT WE DO</h2>
+            <div>
+                    See our work in action. We are ready to improve and grow your business and help achieve expected success.
+                    
+                    Offering innovation and flexibility, quick and high result-orinted work. Get the skills you need to stay competitive in the world of business.</div>
+            <div>
+                <ul>
+                    <li>Business Plan 5yr</li>
+                    <li>Expert Business Plans</li>
+                    <li>International Expansion Advice</li>
+                    <li>Franchise Assessment</li>
+                    <li>Opportunities for Growth Assessment</li>
+                    <li>Business Plan 3yr</li>
+                    <li> International business services</li>
+                 </ul>
+            </div>
+            <div>
+                <ul>
+                    <li>Market Research</li>
+                    <li>Sales Analysis & Pricing Analysis</li>
+                    <li>Advertising</li>
+                    <li>Lead Generation</li>
+                    <li>Online Marketing Advice</li>
+                    <li>Marketing Plan 1yr</li>
+                    <li>International Trading Glossary</li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li>Online Store</li>
+                    <li>Online Marketing</li>
+                    <li>Search Engine Optimization</li>
+                    <li>Using Search Engines</li>
+                    <li>Selling Globally Online</li>
+                    <li>Website Design</li>
+                    <li>Foreign Exchange Online</li>
+                </ul>
+            </div>
+        </div>
+        <div class = "events">
+            <h2>EVENTS</h2>
+            <p>We know what it takes to be the leader!</p>
+            <p>Your prosperity is our goal!</p>
+            <figure>
+                <img src="Holderlin/img/page1-img1.jpg" alt="">
+                <figcaption><p>Since September 3 we have been waiting for our Ukrainian guests. Our new office was opened in Kyiv. We will be happy to meet you and topropose our help to you. Don't be afraid to begin a new happy life. Welcome!</p>
+                        <p><a href = "#">Learn more</a></p>
+                </figcaption>
+            </figure>
+            <figure>
+                <img src="Holderlin/img/page1-img2.jpg" alt="">
+                <figcaption>
+                        <p>This year fall company business-school will join ambitious. purposeful people in New York. You can spend your time profitably with us from 26th October till 7th November.It's not late to register yet.</p>
+                        <p><a href = "#">Learn more</a></p>
+                </figcaption>
+            </figure>
+            <figure>
+                <img src="Holderlin/img/page1-img3.jpg" alt="">
+                <figcaption>
+                        <p>The 20th course of the up-grade program has just finiched and 87 participants from 49 countries are now ready to move more masterfully on the career ladder. We are pround of our learners and sure that they will achieve their goals.</p>
+                        <p><a href = "#">Learn more</a></p>
+                </figcaption>
+            </figure>
+        </div>
+        <div class = "programs">
+            <ul>
+                <li id = "upgrade">
+                    <h2>UP-GRADE PROGRAM</h2> 
+                    <p>Our company up-grade program is for you! With the help of the best lectures and profs you will improve requisite skills.</p>
+                </li>
+                <li id = "business">
+                    <h2>BUSINESS STRATEGY</h2>
+                    <p>Offering innovation and flexibility, quick and high result-oriented work. Get the skills you need to stay competitive.</p>
+                </li>
+            </ul>
+            <ul>
+                <li id = "education">
+                    <h2>EDUCATION & COURSES</h2>
+                    <p>Our dedicated expert staff provides you with the superfast support services to help individuals and business.</p>
+                </li>
+                <li id = "bestresult">
+                    <h2>THE BEST RESULT</h2>
+                    <p>We want to share with the world fresh business ideas which have a great future, can make your business more produstive.</p>
+                </li>
+            </ul>
+            <ul>
+                <li id = "business-creation">
+                    <h2>BUSINESS-CREATION PROGRAM</h2>
+                    <p>Successful businessmen will explain you how to develop new business step by step and to create prosperous product. </p>
+                </li>
+                <li id = "customer">
+                    <h2>CUSTOMER CARE</h2>
+                    <p>We provide professional customer care and excellent consulting services to build your business growth & prosperity. </p>
+                </li>
+            </ul>
+        </div>
+        <div class = "projects">
+            <h2>OUR PROJECTS</h2>
+            <p>Extensive sales and marketing experiense.</p>
+            <p>Progressive business thinking.</p>
+            <div>
+                <figure><img src="Holderlin/img/img1.jpg" alt=""></figure>
+                <figure><img src="Holderlin/img/img2.jpg" alt=""></figure>
+                <figure><img src="Holderlin/img/img3.jpg" alt=""></figure>
+                <figure><img src="Holderlin/img/img4.jpg" alt=""></figure>
+                <figure><img src="Holderlin/img/img5.jpg" alt=""></figure>
+                <figure><img src="Holderlin/img/img6.jpg" alt=""></figure>
+            </div>
+            <div id = "viewall">VIEW ALL</div>
+        </div>
+        <div class = "partners">
+            <ul>
+                <li><img src="Holderlin/img/partnername1.png" alt=""></li>
+                <li><img src="Holderlin/img/partnername2.png" alt=""></li>
+                <li><img src="Holderlin/img/partnername3.png" alt=""></li>
+                <li><img src="Holderlin/img/partnername4.png" alt=""></li>
+                <li><img src="Holderlin/img/partnername5.png" alt=""></li>
+                <li><img src="Holderlin/img/partnername6.png" alt=""></li>
+                <li><img src="Holderlin/img/partnername7.png" alt=""></li>
+            </ul>
+        </div>
+    </main>
+    <footer>
+        <p> Holderlin &copy; Privacy policy</p>
+        <p>Solutions | Resources | Customer Services | Partnering</p>
+    </footer>
+</body>
+</html>
